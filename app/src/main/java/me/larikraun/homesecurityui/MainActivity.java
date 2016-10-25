a20f4e5d40954d3dbc414cc9912a813af8d23ed7
omolara <larryade92@gmail.com>
2016-10-25 15:43:14 +0100

    [Feature] bound onclickListener to keypads

--- app/src/main/java/me/larikraun/homesecurityui/MainActivity.java
+++ app/src/main/java/me/larikraun/homesecurityui/MainActivity.java
@@ -10,14 +10,18 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 	ListView keyHolderList;
 	CustomAdapter mCustomAdapter;
 	ImageView mImageView;
 	View v2;
+	TextView cancel;
+	ArrayList<Integer> passFields = new ArrayList<> ();
+	ArrayList<Integer> numberFields = new ArrayList<> ();
 
 	@TargetApi(Build.VERSION_CODES.LOLLIPOP)
 	@Override
@@ -34,24 +38,35 @@ public class MainActivity extends AppCompatActivity {
 		keyHolderModels.add (keyHolder2);
 		keyHolderModels.add (keyHolder3);
 		keyHolderModels.add (keyHolder4);
+
+		passFields.add (R.id.pass_1);
+		passFields.add (R.id.pass_2);
+		passFields.add (R.id.pass_3);
+		passFields.add (R.id.pass_4);
+
+		numberFields.add (R.id.no_0);
+		numberFields.add (R.id.no_1);
+		numberFields.add (R.id.no_2);
+		numberFields.add (R.id.no_3);
+		numberFields.add (R.id.no_4);
+		numberFields.add (R.id.no_5);
+		numberFields.add (R.id.no_6);
+		numberFields.add (R.id.no_7);
+		numberFields.add (R.id.no_8);
+		numberFields.add (R.id.no_9);
+
 		mCustomAdapter = new CustomAdapter (this, keyHolderModels);
 		keyHolderList = (ListView) findViewById (R.id.keyHoldersList);
 		mImageView = (ImageView) findViewById (R.id.lock);
 		keyHolderList.setAdapter (mCustomAdapter);
-		mImageView.setOnClickListener (new View.OnClickListener () {
-			@Override
-			public void onClick (View v) {
-				LayoutInflater inflater = LayoutInflater.from (MainActivity.this);
-				v2 = inflater.inflate (R.layout.lock_screen, null, false);
-				v2.setOnClickListener (new View.OnClickListener () {
-					@Override
-					public void onClick (View v) {
+		mImageView.setOnClickListener (this);LayoutInflater inflater = LayoutInflater.from (MainActivity.this);
+		v2 = inflater.inflate (R.layout.lock_screen, null, false);
+		cancel = (TextView) v2.findViewById (R.id.cancel);
+		cancel.setOnClickListener (this);
 
-					}
-				});
-				addContentView (v2, new LinearLayout.LayoutParams (ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-			}
-		});
+		for (int i : numberFields) {
+			v2.findViewById (i).setOnClickListener (this);
+		}
 	}
 
 	@Override
@@ -60,6 +75,52 @@ public class MainActivity extends AppCompatActivity {
 			((ViewGroup) v2.getParent ()).removeView (v2);
 		} else {
 			super.onBackPressed ();
+		}
+	}
+
+	@Override
+	public void onClick (View v) {
+		int id = v.getId ();
+		switch (id) {
+			case R.id.cancel:
+				clearPassFields ();
+				break;
+			case R.id.lock:
+
+				v2.setOnClickListener (new View.OnClickListener () {
+					@Override
+					public void onClick (View v) {
+						//deliberately left blank
+					}
+				});
+				addContentView (v2, new LinearLayout.LayoutParams (ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+				break;
+			case R.id.no_0:
+			case R.id.no_1:
+			case R.id.no_2:
+			case R.id.no_3:
+			case R.id.no_4:
+			case R.id.no_5:
+			case R.id.no_6:
+			case R.id.no_7:
+			case R.id.no_8:
+			case R.id.no_9:
+				fillPassFields ();
+		}
+	}
+
+	private void clearPassFields () {
+		for (int i : passFields) {
+			v2.findViewById (i).setBackground (getResources ().getDrawable (R.drawable.white_circular_border, null));
+		}
+	}
+
+	int count = 0;
+
+	private void fillPassFields () {
+		if (count < 4) {
+			v2.findViewById (passFields.get (count)).setBackground (getResources ().getDrawable (R.drawable.white_circular_fill));
+			count++;
 		}
 	}
 }
