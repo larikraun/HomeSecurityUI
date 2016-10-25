430362fc379348fd9f76cb34a86929c7972eb5f6
omolara <larryade92@gmail.com>
2016-10-25 14:24:59 +0100

    [Feature] second screen with custom Keyboard

--- app/src/main/java/me/larikraun/homesecurityui/MainActivity.java
+++ app/src/main/java/me/larikraun/homesecurityui/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
 	ListView keyHolderList;
 	CustomAdapter mCustomAdapter;
 	ImageView mImageView;
+	View v2;
 
 	@TargetApi(Build.VERSION_CODES.LOLLIPOP)
 	@Override
@@ -26,9 +27,9 @@ public class MainActivity extends AppCompatActivity {
 		setContentView (R.layout.activity_main);
 		ArrayList<KeyHolderModel> keyHolderModels = new ArrayList<> ();
 		KeyHolderModel keyHolder1 = new KeyHolderModel ("Omolara Adejuwon", "Everyday", "12:00pm to 3:00pm", "1");
-		KeyHolderModel keyHolder2 = new KeyHolderModel ("Omolara Adejuwon", "Everyday", "12:00pm to 3:00pm", "0");
-		KeyHolderModel keyHolder3 = new KeyHolderModel ("Omolara Adejuwon", "Everyday", "12:00pm to 3:00pm", "1");
-		KeyHolderModel keyHolder4 = new KeyHolderModel ("Omolara Adejuwon", "Everyday", "12:00pm to 3:00pm", "1");
+		KeyHolderModel keyHolder2 = new KeyHolderModel ("Oreoluwa Adejuwon", "Everyday", "12:00pm to 3:00pm", "0");
+		KeyHolderModel keyHolder3 = new KeyHolderModel ("Adejumoke Adejuwon", "Everyday", "12:00pm to 3:00pm", "1");
+		KeyHolderModel keyHolder4 = new KeyHolderModel ("Adeyimika Adejuwon", "Everyday", "12:00pm to 3:00pm", "1");
 		keyHolderModels.add (keyHolder1);
 		keyHolderModels.add (keyHolder2);
 		keyHolderModels.add (keyHolder3);
@@ -41,9 +42,24 @@ public class MainActivity extends AppCompatActivity {
 			@Override
 			public void onClick (View v) {
 				LayoutInflater inflater = LayoutInflater.from (MainActivity.this);
-				View v2 = inflater.inflate (R.layout.lock_screen, null, false);
+				v2 = inflater.inflate (R.layout.lock_screen, null, false);
+				v2.setOnClickListener (new View.OnClickListener () {
+					@Override
+					public void onClick (View v) {
+
+					}
+				});
 				addContentView (v2, new LinearLayout.LayoutParams (ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
 			}
 		});
+	}
+
+	@Override
+	public void onBackPressed () {
+		if (v2 != null) {
+			((ViewGroup) v2.getParent ()).removeView (v2);
+		} else {
+			super.onBackPressed ();
+		}
 	}
 }
